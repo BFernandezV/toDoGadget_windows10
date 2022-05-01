76afef067642aab2364abaa96e84793cb702b794
DESKTOP-H4UPH14\Benjamín Fernández <bfernandez@inf.udec.cl>
2022-04-30 22:12:09 -0400

    Commit Inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import "./App.css";
+
+function App() {
+  //Seteamos el estilo de la ventana
+
+  return (
+    <div className="App">
+      <h1 className="m-0">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
